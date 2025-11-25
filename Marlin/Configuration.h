2569b9f34afcdf04ef796b2fec2f24199d98f050
Marius Beul <marius.beul@energy-robotics.com>
2025-11-25 09:49:51 +0100

    Classic Jerk

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1243,10 +1243,10 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-//#define CLASSIC_JERK
+#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
-  #define DEFAULT_XJERK 10.0
-  #define DEFAULT_YJERK 10.0
+  #define DEFAULT_XJERK 5.0
+  #define DEFAULT_YJERK 5.0
   #define DEFAULT_ZJERK  0.3
   //#define DEFAULT_IJERK  0.3
   //#define DEFAULT_JJERK  0.3
